61c17b198427d979a98d268f0509627afe2323ca
Nicolas Danelon <nicolasdanelon@gmail.com>
2019-07-10 21:25:18 -0300

    sidebar theft & life cycle events

---
+++ pepito.js
@@ -0,0 +1,12 @@
+const ar = [
+    'one',
+    2,
+    null,
+    false,
+    2.2,
+];
+
+for(let me of ar) {
+   console.log(me);
+}
+
--- src/App.js
+++ src/App.js
@@ -1,26 +1,97 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Container, Header, Button,
+   Icon, Image, Menu,
+   Segment, Sidebar } from 'semantic-ui-react'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+let lastScrollY = 0;
+let ticking = false;
+
+class App extends React.Component {
+  state = { visible: false }
+
+  handleHideClick = () => this.setState({ visible: false })
+  handleShowClick = () => this.setState({ visible: true })
+  handleSidebarHide = () => this.setState({ visible: false })
+  // constructo(props) {
+  //   super(props);
+  //   this.handleScroll = this.handleScroll.bind(this);
+  // }
+
+  componentDidMount() {
+    window.addEventListener('scroll', this.handleScrollW);
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener('scroll', this.handleScroll);
+  }
+
+  nav = React.createRef();
+
+  handleScroll = () => {
+    lastScrollY = window.scrollY;
+
+    if (!ticking) {
+      window.requestAnimationFrame(() => {
+        this.nav.current.style.top = `${lastScrollY}px`;
+        console.log(`${lastScrollY}px`);
+        ticking = false;
+      });
+
+      ticking = true;
+    }
+  };
+
+  render() {
+    const { visible } = this.state
+
+    return (
+      <Container text>
+        <Header as='h2'>Header</Header>
+
+        <Button.Group>
+          <Button disabled={visible} onClick={this.handleShowClick}>
+            Show sidebar
+          </Button>
+          <Button disabled={!visible} onClick={this.handleHideClick}>
+            Hide sidebar
+          </Button>
+        </Button.Group>
+
+        <Sidebar.Pushable as={Segment} style={{ height: 1900 }}>
+          <Sidebar
+            as={Menu}
+            animation='overlay'
+            icon='labeled'
+            inverted
+            onHide={this.handleSidebarHide}
+            vertical
+            visible={visible}
+            width='thin'
+          >
+            <Menu.Item as='a'>
+              <Icon name='home' />
+              Home
+            </Menu.Item>
+            <Menu.Item as='a'>
+              <Icon name='gamepad' />
+              Games
+            </Menu.Item>
+            <Menu.Item as='a'>
+              <Icon name='camera' />
+              Channels
+            </Menu.Item>
+          </Sidebar>
+
+          <Sidebar.Pusher>
+            <Segment basic>
+              <Header as='h3'>Application Content</Header>
+              <Image src='https://react.semantic-ui.com//images/wireframe/paragraph.png' />
+            </Segment>
+          </Sidebar.Pusher>
+        </Sidebar.Pushable>
+      </Container>
+    );
+  }
 }
 
 export default App;
